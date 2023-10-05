300fbc90aaef322d78601cfa6e65da2aca5a9894
oussama134 <https://github.com/oussama134/alx-low_level_programming>
2023-10-05 16:11:14 -0700

    stay

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19"); 
+		return (0) ;
+} 
+
