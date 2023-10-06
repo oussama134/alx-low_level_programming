87197fb11615cadf52f831b66b28df38578db771
oussama134 <https://github.com/oussama134/alx-low_level_programming>
2023-10-06 10:34:59 -0700

    nsa

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: checks on the last digit of the assigned var
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n % 10;
+	if (m > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	}
+		else if ( m == 0)
+		{
+			printf(" last digit of %d is %d and the string is 0\n", n, m);
+		}
+	else 
+	{
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	} 
+	return (0);
+}
+
+
+
