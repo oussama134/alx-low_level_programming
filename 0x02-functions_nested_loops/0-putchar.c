a6fc5d6abdf0755ba249660ef5906e5d052eae9b
oussama134 <https://github.com/oussama134/alx-low_level_programming>
2023-10-10 09:58:06 -0700

    slatan

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
 include<stdio.h>
 /**
  *
- * main - Entry point
+ * main - print putchar 
  *
- * Return: always 0 (Success)
+ * Return: 0
  */
-int main() 
+int main(void)
 {
-	my name = oussama ;
-	putchar(my name);
-	return (0):
-} 
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char lettre;
+	int i;
+	for ( i=0; i<10; i++){
+		for (lettre = 'a'; lettre <= 'z'; i++){
+			_putchar(lettre);
+			_putchar('\n'); }
+	}
+
+} 
+
