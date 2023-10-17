faf294454ef9e19d6cfb34e69541c851c3e80987
oussama134 <https://github.com/oussama134/alx-low_level_programming>
2023-10-17 09:58:37 -0700

    nsaa

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -8,11 +7,13 @@
 
 void print_numbers(void)
 {
-	char = c;
-	for ( c=0; c<10; c++)
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
 }
-	
+
+
